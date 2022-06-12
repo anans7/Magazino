d12ae0df518ae0fcdd346830be5ba099ee3e60b9
anans <ananasjames7@gmail.com>
2022-06-12 08:27:20 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,188 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import { useState, useEffect, useCallback } from "react";
+
+
+
+
+import Web3 from "web3";
+import { newKitFromWeb3 } from "@celo/contractkit";
+import BigNumber from "bignumber.js";
+import IERC from "./contract/IERC.abi.json";
+import Magazino from  './contract/Magazino.abi.json';
+import Magazines from './components/Magazines';
+import NewMagazine from './components/NewMagazine';
+
+
+const ERC20_DECIMALS = 18;
+
+
+const contractAddress = "0x1AEfc0fAb8Bf843A4858Ae7544F6B36A68923FB8";
+const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
+
+
+
+
+function App() {
+
+  const [contract, setcontract] = useState(null);
+  const [address, setAddress] = useState(null);
+  const [kit, setKit] = useState(null);
+  const [cUSDBalance, setcUSDBalance] = useState(0);
+  const [magazines, setMagazines] = useState([]);
+
+  const connectToWallet = async () => {
+    if (window.celo) {
+      try {
+        await window.celo.enable();
+        const web3 = new Web3(window.celo);
+        let kit = newKitFromWeb3(web3);
+
+        const accounts = await kit.web3.eth.getAccounts();
+        const user_address = accounts[0];
+
+        kit.defaultAccount = user_address;
+
+        await setAddress(user_address);
+        await setKit(kit);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log("Error Occurred");
+      
+     }
+   };
+ 
+    
+   const getBalance = (async () => {
+    try {
+      const balance = await kit.getTotalBalance(address);
+      const USDBalance = balance.cUSD.shiftedBy(-ERC20_DECIMALS).toFixed(2);
+      const contract = new kit.web3.eth.Contract(Magazino, contractAddress);
+      setcontract(contract);
+      setcUSDBalance(USDBalance);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+
+  
+  const unpublishMagazine = async (_index) => {
+    try {
+      await contract.methods.unpublishMagazine(_index).send({ from: address });
+      getMagazine();
+      getBalance();
+    } catch (error) {
+      alert(error);
+    }};
+   
+
+ 
+   
+   const getMagazine =  async () => {
+     const magazinesLength = await contract.methods.getmagazinesLength().call();
+     console.log(magazinesLength);
+     const _magazinn = []
+     for (let index = 0; index < magazinesLength; index++) {
+       console.log(magazinesLength);
+       let _magazines = new Promise(async (resolve, reject) => {
+       let magazine = await contract.methods.getMagazine(index).call();
+
+       resolve({
+        index: index,
+        owner: magazine[0],
+        image: magazine[1],
+        name: magazine[2],
+        edition: magazine[3],
+       price: magazine[4],
+       magazineid: magazine[5]
+
+                 
+      });
+    });
+    _magazinn.push(_magazines);
+  }
+  const _magazines = await Promise.all(_magazinn);
+  setMagazines(_magazines);
+  console.log(magazines)
+  
+};
+
+
+useEffect(() => {
+  connectToWallet();
+}, []);
+
+useEffect(() => {
+  if (kit && address) {
+    getBalance();
+   
+  }
+}, [kit, address]);
+
+useEffect(() => {
+  if (contract) {
+    getMagazine();
+  }
+}, [contract]);  
+
+
+const addMagazine = async (
+  _image,
+  _name,
+  _edition,
+  price
+) => {
+
+  const _price = new BigNumber(price).shiftedBy(ERC20_DECIMALS).toString();
+    try {
+      await contract.methods
+        .addMagazine(_image, _name, _edition, _price)
+        .send({ from: address });
+       getMagazine();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const buyMagazine = async (_index,) => {
+    try {
+      const cUSDContract = new kit.web3.eth.Contract(IERC, cUSDContractAddress);
+    
+      
+      await cUSDContract.methods
+        .approve(contractAddress, magazines[_index].price)
+        .send({ from: address });
+      await contract.methods.buyMagazine(_index).send({ from: address });
+      getMagazine();
+      getBalance();
+    } catch (error) {
+      console.log(error)
+    }};
+
+  
+
+
+
+  return (
+    <div>
+      <Navbar balance = {cUSDBalance} />
+      <Magazines magazines ={magazines}
+      buyMagazine = {buyMagazine}
+      unpublishMagazine = {unpublishMagazine}
+       
+       
+      
+       
+      />
+       <NewMagazine addMagazine = {addMagazine}
+       
+/>
+    </div>
+    )
+  }
+
+export  default App;
---
+++ src/contract/Magazino.sol
@@ -0,0 +1,132 @@
+   // SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+interface IERC20Token {
+   function transfer(address, uint256) external returns (bool);
+
+    function approve(address, uint256) external returns (bool);
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) external returns (bool);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address) external view returns (uint256);
+
+    function allowance(address, address) external view returns (uint256);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+contract Magazino {
+     uint internal magazinesLength = 0;
+      address internal cUsdTokenAddress =    0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+       uint256 internal magazinesId = 0;
+
+
+        event unpublishMagazineEvent(uint256 magazineId);
+
+
+
+         
+
+       struct Magazine{
+           address payable owner;
+           string image;
+           string name;
+            string edition;
+            uint price;
+            uint256 magazineId;
+       }
+
+        mapping (uint =>  Magazine) internal magazines;
+
+        function getMagazine(uint _index) public view returns (
+        address payable,
+        string memory,
+        string memory,
+        string memory,
+        uint,
+        uint256
+
+
+         ) {
+        return (  
+            magazines[_index].owner,
+             magazines[_index].image,
+              magazines[_index].name,
+               magazines[_index].edition,
+                magazines[_index].price,
+                 magazines[_index].magazineId
+
+                 
+        );
+       
+    }
+
+
+           function addMagazine (
+        string memory _image,
+        string memory _name,
+        string memory _edition,
+        uint _price
+         ) public {
+
+              magazines [magazinesLength] =  Magazine(
+            payable(msg.sender),
+             _image,
+             _name,
+             _edition,
+             _price,
+             magazinesId
+
+              );
+
+                magazinesLength++;
+          magazinesId++;
+         }
+
+
+          function buyMagazine(uint _index) public payable  {
+        require(
+          IERC20Token(cUsdTokenAddress).transferFrom(
+            msg.sender,
+            magazines[_index].owner,
+            magazines[_index].price
+          ),
+          "Transfer failed."
+        );
+
+        
+         
+    }
+
+
+     function unpublishMagazine(uint _index) external {
+	         require(msg.sender == magazines[_index].owner, "Only owner can unublish magazine");      
+            uint256 magazineId = magazines[_index].magazineId;      
+            magazines[_index] = magazines[magazinesLength - 1];
+            delete magazines[magazinesLength - 1];
+                  
+                  magazinesLength--; 
+                   emit unpublishMagazineEvent(magazineId);
+                   
+}
+
+  function getmagazinesLength() public view returns (uint) {
+        return (magazinesLength);
+  }
+
+
+}
+
