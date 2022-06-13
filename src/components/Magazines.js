f3baf5e8eed5c2c3ec0ada16b9431c78c08f42c8
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-06-13 23:31:13 +0400

    Fixed frontend issues and improved security on smart contract

--- src/components/Magazines.js
+++ src/components/Magazines.js
@@ -1,50 +1,48 @@
-import React from 'react';
- 
- 
+import React from "react";
 
 const Magazines = (props) => {
   return (
-    <div className='container'> 
-    <div className="row">
-    {props.magazines.map((mag) => (
-      <div className= "col-3">
-        <div class="card"  key={mag.index}>
-
-        <div class="card-img">
-        <img class="img-fluid" src={mag.image} alt=" img top"/>
-         
-        </div>
-
-        <div class="card-section"> magazine Name : {mag.name}
-        </div>
-        <div class="card-section"><h3> Magazine Edition: {mag.edition}</h3>
-        </div>
-
-        <div class="card-bottom"><h3> Price : {mag.price /1000000000000000000}cUSD</h3></div>
-</div>
-
-<div> 
-       <button type="button" class="btn btn-outline-dark" onClick={() => props.buyMagazine(mag.index)}>Buy Now</button>
-       
-       <button type='button' class="btn btn-outline-dark" onClick={ ()=> props.unpublishMagazine(mag.index)}>unpublish Magazine</button>
-                
-                </div>
-                </div>
-
-                ))};
-
-</div>
-</div>
-
-
-)
-}
-
-
-
-
-
-
-
+    <div className="container">
+      <div className="d-flex flex-wrap">
+        {props.magazines.map((mag) => (
+          <div className="mx-2 my-2" key={mag.index}>
+            <div className="card" style={{ width: "18rem" }}>
+              <img
+                className="card-img-top"
+                src={mag.image}
+                alt=" img top"
+                style={{ maxHeight: "20rem" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{mag.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Magazine Edition: {mag.edition}
+                </h6>
+                <p className="card-text">
+                  Price : {mag.price / 1000000000000000000}cUSD
+                </p>
+                <button
+                  type="button"
+                  className="btn btn-outline-dark"
+                  onClick={() => props.buyMagazine(mag.index)}
+                >
+                  Buy Now
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-outline-dark mt-3"
+                  onClick={() => props.unpublishMagazine(mag.index)}
+                >
+                  unpublish Magazine
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+        ;
+      </div>
+    </div>
+  );
+};
 
 export default Magazines;
--- src/contract/Magazino.sol
+++ src/contract/Magazino.sol
@@ -1,18 +1,15 @@
-   // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity >=0.7.0 <0.9.0;
 
 
 interface IERC20Token {
-   function transfer(address, uint256) external returns (bool);
+
+    function transfer(address, uint256) external returns (bool);
 
     function approve(address, uint256) external returns (bool);
 
-    function transferFrom(
-        address,
-        address,
-        uint256
-    ) external returns (bool);
+    function transferFrom(address, address, uint256) external returns (bool);
 
     function totalSupply() external view returns (uint256);
 
@@ -21,112 +18,88 @@ interface IERC20Token {
     function allowance(address, address) external view returns (uint256);
 
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 contract Magazino {
-     uint internal magazinesLength = 0;
-      address internal cUsdTokenAddress =    0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
-       uint256 internal magazinesId = 0;
 
+    uint internal magazinesLength = 0;
+    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+    uint256 internal magazinesId = 0;
+    
+    event unpublishMagazineEvent(uint256 magazineId);
 
-        event unpublishMagazineEvent(uint256 magazineId);
+    struct Magazine{
+        address payable owner;
+        string image;
+        string name;
+        string edition;
+        uint price;
+        uint256 magazineId;
+        
+    }
 
+    mapping (uint =>  Magazine) internal magazines;
 
-
-         
-
-       struct Magazine{
-           address payable owner;
-           string image;
-           string name;
-            string edition;
-            uint price;
-            uint256 magazineId;
-       }
-
-        mapping (uint =>  Magazine) internal magazines;
-
-        function getMagazine(uint _index) public view returns (
-        address payable,
-        string memory,
-        string memory,
-        string memory,
-        uint,
-        uint256
-
-
-         ) {
-        return (  
-            magazines[_index].owner,
-             magazines[_index].image,
-              magazines[_index].name,
-               magazines[_index].edition,
+    function getMagazine(uint _index) public view returns 
+    (address payable, string memory, string memory, string memory, uint, uint256){
+        require(_index >= 0, "Enter a valid index");
+        return  (  
+                magazines[_index].owner,
+                magazines[_index].image,
+                magazines[_index].name,
+                magazines[_index].edition,
                 magazines[_index].price,
-                 magazines[_index].magazineId
-
-                 
-        );
+                magazines[_index].magazineId
+                );
        
     }
 
 
-           function addMagazine (
-        string memory _image,
-        string memory _name,
-        string memory _edition,
-        uint _price
-         ) public {
-
-              magazines [magazinesLength] =  Magazine(
-            payable(msg.sender),
-             _image,
-             _name,
-             _edition,
-             _price,
-             magazinesId
-
-              );
-
-                magazinesLength++;
-          magazinesId++;
-         }
+    function addMagazine (string memory _image, string memory _name, string memory _edition, uint _price) public {
+        require(bytes(_image).length > 0, "Enter valid image url");
+        require(bytes(_name).length > 0, "Enter valid name");
+        require(bytes(_edition).length > 0, "Enter a valid edition");
+        require(_price > 0, "Enter a valid price");
+        magazines [magazinesLength] =  Magazine(
+                    payable(msg.sender),
+                    _image,
+                    _name,
+                    _edition,
+                    _price,
+                    magazinesId
+                    );
+        magazinesLength++;
+        magazinesId++;
+    }
 
 
-          function buyMagazine(uint _index) public payable  {
+    function buyMagazine(uint _index) public payable  {
+        require(msg.sender != magazines[_index].owner, "You can't buy your own magazine");
+        require(_index >= 0, "Enter a valid index");
         require(
-          IERC20Token(cUsdTokenAddress).transferFrom(
+            IERC20Token(cUsdTokenAddress).transferFrom(
             msg.sender,
             magazines[_index].owner,
             magazines[_index].price
-          ),
-          "Transfer failed."
-        );
-
-        
+                ), "Transfer failed.");
          
     }
 
 
-     function unpublishMagazine(uint _index) external {
-	         require(msg.sender == magazines[_index].owner, "Only owner can unublish magazine");      
-            uint256 magazineId = magazines[_index].magazineId;      
-            magazines[_index] = magazines[magazinesLength - 1];
-            delete magazines[magazinesLength - 1];
-                  
-                  magazinesLength--; 
-                   emit unpublishMagazineEvent(magazineId);
-                   
-}
+    function unpublishMagazine(uint _index) external {
+        require(_index >= 0, "Enter a valid index");
+	    require(msg.sender == magazines[_index].owner, "Only owner can unpublish magazine");      
+        uint256 magazineId = magazines[_index].magazineId;      
+        magazines[_index] = magazines[magazinesLength - 1];
+        delete magazines[magazinesLength - 1];              
+        magazinesLength--; 
+        emit unpublishMagazineEvent(magazineId);               
+    }
 
-  function getmagazinesLength() public view returns (uint) {
+    function getmagazinesLength() public view returns (uint) {
         return (magazinesLength);
-  }
+    }
 
 
 }
-
