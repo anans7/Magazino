cba63ff73bb0ad5344518544d86dad7ca8941145
Daniel Ogbuti <danielogbuti@gmail.com>
2022-06-15 06:29:32 +0100

    feat: added events

--- src/contract/Magazino.sol
+++ src/contract/Magazino.sol
@@ -1,10 +1,9 @@
-   // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity >=0.7.0 <0.9.0;
 
-
 interface IERC20Token {
-   function transfer(address, uint256) external returns (bool);
+    function transfer(address, uint256) external returns (bool);
 
     function approve(address, uint256) external returns (bool);
 
@@ -29,104 +28,100 @@ interface IERC20Token {
 }
 
 contract Magazino {
-     uint internal magazinesLength = 0;
-      address internal cUsdTokenAddress =    0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
-       uint256 internal magazinesId = 0;
+    uint internal magazinesLength = 0;
+    address internal cUsdTokenAddress =
+        0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+    uint256 internal magazinesId = 0;
 
+    event unpublishMagazineEvent(uint256 magazineId);
+    event createMagazineEvent(
+        string image,
+        string name,
+        string edition,
+        uint price
+    );
 
-        event unpublishMagazineEvent(uint256 magazineId);
-
-
-
-         
-
-       struct Magazine{
-           address payable owner;
-           string image;
-           string name;
-            string edition;
-            uint price;
-            uint256 magazineId;
-       }
-
-        mapping (uint =>  Magazine) internal magazines;
-
-        function getMagazine(uint _index) public view returns (
-        address payable,
-        string memory,
-        string memory,
-        string memory,
-        uint,
-        uint256
-
-
-         ) {
-        return (  
-            magazines[_index].owner,
-             magazines[_index].image,
-              magazines[_index].name,
-               magazines[_index].edition,
-                magazines[_index].price,
-                 magazines[_index].magazineId
-
-                 
-        );
-       
+    struct Magazine {
+        address payable owner;
+        string image;
+        string name;
+        string edition;
+        uint price;
+        uint256 magazineId;
+        uint createdAt;
     }
 
+    mapping(uint => Magazine) internal magazines;
 
-           function addMagazine (
+    function getMagazine(uint _index)
+        public
+        view
+        returns (
+            address payable,
+            string memory,
+            string memory,
+            string memory,
+            uint256,
+            uint256,
+            uint256
+        )
+    {
+        return (
+            magazines[_index].owner,
+            magazines[_index].image,
+            magazines[_index].name,
+            magazines[_index].edition,
+            magazines[_index].price,
+            magazines[_index].magazineId,
+            magazines[_index].createdAt
+        );
+    }
+
+    function addMagazine(
         string memory _image,
         string memory _name,
         string memory _edition,
         uint _price
-         ) public {
-
-              magazines [magazinesLength] =  Magazine(
+    ) public {
+        magazines[magazinesLength] = Magazine(
             payable(msg.sender),
-             _image,
-             _name,
-             _edition,
-             _price,
-             magazinesId
-
-              );
-
-                magazinesLength++;
-          magazinesId++;
-         }
-
-
-          function buyMagazine(uint _index) public payable  {
-        require(
-          IERC20Token(cUsdTokenAddress).transferFrom(
-            msg.sender,
-            magazines[_index].owner,
-            magazines[_index].price
-          ),
-          "Transfer failed."
+            _image,
+            _name,
+            _edition,
+            _price,
+            magazinesId,
+            block.timestamp
         );
-
-        
-         
+        emit createMagazineEvent(_image, _name, _edition, _price);
+        magazinesLength++;
+        magazinesId++;
     }
 
+    function buyMagazine(uint _index) public payable {
+        require(
+            IERC20Token(cUsdTokenAddress).transferFrom(
+                msg.sender,
+                magazines[_index].owner,
+                magazines[_index].price
+            ),
+            "Transfer failed."
+        );
+    }
 
-     function unpublishMagazine(uint _index) external {
-	         require(msg.sender == magazines[_index].owner, "Only owner can unublish magazine");      
-            uint256 magazineId = magazines[_index].magazineId;      
-            magazines[_index] = magazines[magazinesLength - 1];
-            delete magazines[magazinesLength - 1];
-                  
-                  magazinesLength--; 
-                   emit unpublishMagazineEvent(magazineId);
-                   
-}
+    function unpublishMagazine(uint _index) external {
+        require(
+            msg.sender == magazines[_index].owner,
+            "Only owner can unpublish magazine"
+        );
+        uint256 magazineId = magazines[_index].magazineId;
+        magazines[_index] = magazines[magazinesLength - 1];
+        delete magazines[magazinesLength - 1];
 
-  function getmagazinesLength() public view returns (uint) {
+        magazinesLength--;
+        emit unpublishMagazineEvent(magazineId);
+    }
+
+    function getmagazinesLength() public view returns (uint) {
         return (magazinesLength);
-  }
-
-
+    }
 }
-
